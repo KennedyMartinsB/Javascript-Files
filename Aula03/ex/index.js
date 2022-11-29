7a6e51e322dbcc8ce056e8e835d83b60161cda01
Kennedy Martins <kennedy.martins@MFMTI.COM.BR>
2022-11-29 11:50:03 -0300

    Feature: exercicio console

---
+++ Aula03/ex/index.js
@@ -0,0 +1,2 @@
+// Exercicio 01
+console.log("Olá, meu nome é . Estou estudando javascript", 10,"da manhã");
---
+++ Aula03/index.js
@@ -0,0 +1,11 @@
+// Valor literal
+console.log("Olá Mundo")
+
+const msg = 'Mensagem'
+var msg2 = "mensagem"
+
+// Constante
+console.log(msg)
+
+// Var
+console.log(this.msg2)

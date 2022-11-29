f38f58bd00da72ecc685efe7e21e33aeacd83521
KennedyMartinsB <kennedy.martins@bandtec.com.br>
2022-11-29 12:25:12 -0300

    feature: comentarios de bloco

---
+++ Aula03/ex/ex2.js
@@ -0,0 +1,10 @@
+// Escrever um comentario
+// Comentarios são ignorados porem o codigo será executado
+console.log("Olá Mundo") // Outro comentário
+console.log("linha 1")
+
+/*
+    Comentário de bloco
+console.log("Teste")
+console.log("Teste2")
+*/
